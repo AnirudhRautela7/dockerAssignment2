29800ad7abfd76ad5d963877d631588c9937c428
Anirudh <rautelaanirudh7703@gmail.com>
2024-09-06 11:53:19 +0530

    changed path

--- Part 5 - Unit Testing/SELENIUM/Testing/runAllTestSuites.bat
+++ Part 5 - Unit Testing/SELENIUM/Testing/runAllTestSuites.bat
@@ -5,7 +5,7 @@ set mytimestamp=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%-%time:~0,2%%time:~
 echo %mytimestamp%
 
 set reportStyle=Html
-set commonPath=E:\12)DevOps\SELENIUM\Testing
+set commonPath=C:\Users\Anirudh Rautela\Documents\GitHub\CodeGym\Part 5 - Unit Testing\SELENIUM\Testing
 set pythonPath=C:\Users\Anirudh Rautela\AppData\Local\Programs\Python\Python312\python.exe
 
 "%pythonPath%" "%commonPath%\tsReport.py" %reportStyle% "%commonPath%"
