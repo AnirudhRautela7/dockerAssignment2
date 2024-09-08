77d0276888ff462576cafcb8a78bfb9419090a98
Anirudh <rautelaanirudh7703@gmail.com>
2024-09-08 21:43:11 +0530

    "Updated paths in runApp.bat: changed commonPath and pythonPath variables to new locations."

--- Part 5 - Unit Testing/SELENIUM/Testing/runApp.bat
+++ Part 5 - Unit Testing/SELENIUM/Testing/runApp.bat
@@ -5,8 +5,8 @@ set mytimestamp=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%-%time:~0,2%%time:~
 echo %mytimestamp%
 
 set reportStyle="Html"
-set commonPath="E:\12)DevOps\SELENIUM\Testing"
-set pythonPath="F:\python"
+set commonPath="C:\Users\Anirudh Rautela\Desktop\DevOps Handson\dockerAssignment2\Part 5 - Unit Testing\SELENIUM\Testing"
+set pythonPath="C:\Users\Anirudh Rautela\AppData\Local\Programs\Python\Python312"
 
 %pythonPath%\python -m pip install flask
 %pythonPath%\python "E:\12)DevOps\CarbonPipeline\CarbonPipeline\SampleCode\app.py" 
